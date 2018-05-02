74c0f123452855daeae81ae6c0e2a5e5b4a91eff
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2018-05-03 08:20:51 +1200

    add read_asset

---
+++ 1.php
@@ -0,0 +1,32 @@
+<?php
+include 'vendor/autoload.php';
+
+use Strava\API\OAuth;
+use Strava\API\Exception;
+
+try {
+    $options = [
+        'clientId'     => 25225,
+        'clientSecret' => '68ec92ec441b834fa248b397984a81cf462a8b7d',
+        'redirectUri'  => 'http://localhost:8000/1.php'
+    ];
+    $oauth = new OAuth($options);
+
+    if (!isset($_GET['code'])) {
+        print '<a href="'.$oauth->getAuthorizationUrl([
+            // Uncomment required scopes.
+            'scope' => [
+                'public',
+                // 'write',
+                // 'view_private',
+            ]
+        ]).'">Connect</a>';
+    } else {
+        $token = $oauth->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+        print $token->getToken();
+    }
+} catch(Exception $e) {
+    print $e->getMessage();
+}
